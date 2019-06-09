6d1c90ac7deebf3e7ebbffe1d5614a57489196bf
Jeff Anderson <jeff@jeff-anderson.com>
2019-06-09 09:04:56 -0400

    Base functionality working without add.

--- aws-wksp/aws-wksp.go
+++ aws-wksp/aws-wksp.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/workspaces"
+	"log"
 	"os"
 	"sort"
 )
@@ -14,9 +16,8 @@ var awsRegion string
 
 func init() {
 	const (
-		PROFILE   = "profile"
-		REGION    = "region"
-		FILE_NAME = "file"
+		PROFILE = "profile"
+		REGION  = "region"
 	)
 
 	flag.StringVar(&awsProfile, PROFILE, "", "the AWS credentials profile to use")
@@ -27,6 +28,7 @@ func main() {
 
 	listBundlesOp := flag.Bool("list-bundles", false, "list workspace bundles")
 	listWorkspacesOp := flag.Bool("list-workspaces", false, "list workspaces")
+	deleteWorkspacesOp := flag.Bool("delete-workspaces", false, "delete workspaces")
 	fileName := flag.String("file", "", "file to read from or write to")
 
 	flag.Parse()
@@ -43,6 +45,12 @@ func main() {
 	var allBundles []*workspaces.WorkspaceBundle
 	var bundleMap map[string]string
 
+	if *listBundlesOp && *listWorkspacesOp && len(*fileName) > 0 {
+		fmt.Println("specify one of list-bundles or list-workspaces with the file option")
+		flag.Usage()
+		os.Exit(-1)
+	}
+
 	if *listBundlesOp {
 		allBundles = getAllBundles(*svc)
 		if len(*fileName) > 0 {
@@ -51,13 +59,66 @@ func main() {
 			bundleMapPrinter(allBundles)
 		}
 	}
+
 	if *listWorkspacesOp {
 		if len(allBundles) == 0 {
 			allBundles = getAllBundles(*svc)
 		}
 		bundleMap = makeBundleMap(allBundles)
-		workspacePrinter(getWorkspaces(*svc), bundleMap)
+		if len(*fileName) > 0 {
+			writeWorspaceFile(getWorkspaces(*svc), bundleMap, fileName)
+		} else {
+			workspacePrinter(getWorkspaces(*svc), bundleMap)
+		}
 	}
+
+	if *deleteWorkspacesOp && len(*fileName) > 0 {
+		deleteWorkspaces(svc, fileName)
+	}
+}
+
+func deleteWorkspaces(svc *workspaces.WorkSpaces, fileName *string) {
+	f, err := os.Open(*fileName)
+	checkErr(err)
+	defer f.Close()
+
+	r := csv.NewReader(f)
+
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	records = records[1:]
+
+	fmt.Println("\nDELETING WORKSPACES:\n")
+	for _, v := range records {
+		fmt.Printf("%v  %-10v  %v  %v\n", v[0], v[1], v[2], v[3])
+	}
+	fmt.Print("\nThis action is PERMANENT! Type DELETE (in all capital letters) to confirm: ")
+	var answer string
+	fmt.Scanln(&answer)
+	if answer == "DELETE" {
+		deleteWorkspacesOperation(*svc, records)
+	} else {
+		fmt.Println("Workspaces NOT deleted")
+	}
+
+}
+
+func deleteWorkspacesOperation(svc workspaces.WorkSpaces, records [][]string) {
+	requests := make([]*workspaces.TerminateRequest, 0, len(records))
+	for _, v := range records {
+		terminateRequest := new(workspaces.TerminateRequest)
+		terminateRequest.WorkspaceId = &v[0]
+		requests = append(requests, terminateRequest)
+	}
+	input := workspaces.TerminateWorkspacesInput{
+		TerminateWorkspaceRequests: requests,
+	}
+	fmt.Println(input)
+	output, err := svc.TerminateWorkspaces(&input)
+	checkErr(err)
+	fmt.Println(output)
 }
 
 func writeBundleMap(bundles []*workspaces.WorkspaceBundle, fileName *string) {
@@ -139,10 +200,22 @@ func getWorkspaces(svc workspaces.WorkSpaces) []*workspaces.Workspace {
 }
 
 func workspacePrinter(workspaceList []*workspaces.Workspace, bundleMap map[string]string) {
-	fmt.Printf("%-15v Description\n", "Bundle ID")
+	fmt.Printf("Workspace ID, State, User Name, Bundle\n")
 	for _, v := range workspaceList {
 		fmt.Printf("%v  %-10v  %v  %v\n", *v.WorkspaceId, *v.State, *v.UserName, bundleMap[*v.BundleId])
 	}
+}
+
+func writeWorspaceFile(workspaceList []*workspaces.Workspace, bundleMap map[string]string, fileName *string) {
+	f, err := os.Create(*fileName)
+	checkErr(err)
+	defer f.Close()
+
+	fmt.Fprintf(f, "\"workspace_id\",\"state\",\"user_name\",\"bundle\"\n")
+	for _, v := range workspaceList {
+		fmt.Fprintf(f, "\"%v\",\"%v\",\"%v\",\"%v\"\n", *v.WorkspaceId, *v.State, *v.UserName, bundleMap[*v.BundleId])
+	}
+	f.Sync()
 }
 
 func checkErr(e error) {
